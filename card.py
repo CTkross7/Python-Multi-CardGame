18f4d94aaa854bccc3cf7761b97df433a69e2d2c
CTkross7 <junghyeonr2d2@gmail.com>
2025-12-24 02:05:29 +0000

    edit file

--- card.py
+++ card.py
@@ -2,36 +2,50 @@
 from enum import Enum
 
 class Color(Enum):
-    RED = "Red"
-    YELLOW = "Yellow"
-    GREEN = "Green"
-    BLUE = "Blue"
-    WILD = "Wild"
+    RED = "RED"
+    YELLOW = "YELLOW"
+    GREEN = "GREEN"
+    BLUE = "BLUE"
+    WILD = "WILD"
 
 class CardType(Enum):
-    NUMBER = "Number"
-    SKIP = "Skip"
-    REVERSE = "Reverse"
-    DRAW_TWO = "Draw Two"
-    WILD = "Wild"
-    WILD_DRAW_FOUR = "Wild Draw Four"
+    NUMBER = "NUMBER"
+    SKIP = "SKIP"
+    REVERSE = "REVERSE"
+    DRAW_TWO = "DRAW_TWO"
+    WILD = "WILD"
+    WILD_DRAW_FOUR = "WILD_DRAW_FOUR"
 
 class Card:
-    def __init__(self, color, card_type, value=None):
+    def __init__(self, color, ctype, value=None):
         self.color = color
-        self.card_type = card_type
-        self.value = value  # 숫자 카드일 경우만 사용
+        self.type = ctype
+        self.value = value
 
-    def is_playable(self, top_card, current_color):
+    def playable(self, top, current_color):
         if self.color == Color.WILD:
             return True
         return (
             self.color == current_color or
-            self.card_type == top_card.card_type or
-            self.value == top_card.value
+            self.type == top.type or
+            self.value == top.value
+        )
+
+    # ✅ 이 함수가 누락되어 있었음
+    def serialize(self):
+        return {
+            "color": self.color.value,
+            "type": self.type.value,
+            "value": self.value
+        }
+
+    @staticmethod
+    def deserialize(data):
+        return Card(
+            Color(data["color"]),
+            CardType(data["type"]),
+            data["value"]
         )
 
     def __str__(self):
-        if self.card_type == CardType.NUMBER:
-            return f"{self.color.value} {self.value}"
-        return f"{self.color.value} {self.card_type.value}"
+        return f"{self.color.value} {self.type.value} {self.value}"
